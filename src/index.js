83c6d4a3de5a72a2fe4d145233a9e4e41ec338f0
cionz0 <31845603+cionz0@users.noreply.github.com>
2023-09-08 11:06:01 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+/**
+ * This module ...  
+ * 
+ * <!-- do not type below this line -->
+ * Created on: 08/09/23
+ * @project eslint-config
+ * @module src/index
+ * @author cionzo <cionzo@filotrack.com> 
+ */
+ "use strict"
+ 
